f0ccfe343328d584e388558e54eb165fb2b05e1a
Ashutosh Srivastava <ashuindiasri@gmail.com>
2020-07-26 10:02:03 +0530

    portal updates

--- lib/UI/AnimalProfile.dart
+++ lib/UI/AnimalProfile.dart
@@ -1,5 +1,8 @@
+import 'package:Kamadhenu/UI/decorations.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
+import 'package:Kamadhenu/screens/home.dart' as home;
 
 String cattlesID;
 String imageUrl;
@@ -14,7 +17,66 @@ class AnimalProfile extends StatefulWidget {
 class _AnimalProfile extends State<AnimalProfile> {
   final cattleData = Firestore.instance.collection("cattles");
   final userData = Firestore.instance.collection('Users');
-  _showBuyRequestDialogue() {}
+
+  Future<void> _requestSent() {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: Colors.green[50],
+          title: Text(
+            "REQUEST SENT",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> _showBuyRequestDialogue() {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+            title: Row(children: <Widget>[
+              Icon(
+                Icons.info,
+                color: Colors.blue[300],
+              ),
+              Text(" Information")
+            ]),
+            content: Column(
+              children: <Widget>[
+                Text(
+                    "A request will be sent to the owner of the cattle, you need to contact the owner for exchage of money and Livestock, \n\n\nPress OK to continue."),
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    Firestore.instance
+                        .collection('Users')
+                        .document(userID)
+                        .collection('BuyingRequest')
+                        .document()
+                        .setData({
+                      'BuyerID': home.userID,
+                      'AnimalID': cattlesID,
+                    });
+                    _requestSent();
+                  },
+                  color: Colors.blue[100],
+                  child: Text(
+                    "OK",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+            ));
+      },
+    );
+  }
+
   Widget build(context) {
     return Scaffold(
         floatingActionButton: FloatingActionButton(
@@ -42,6 +104,33 @@ class _AnimalProfile extends State<AnimalProfile> {
                 margin: EdgeInsets.all(5),
                 borderOnForeground: true,
                 color: Colors.blue[100],
+              ),
+              Container(
+                decoration: Deco().decoBox(Colors.blue.shade50),
+                child: Column(children: <Widget>[
+                  Deco().titleCon('ANIMAL INFORMATION'),
+                  SizedBox(height: 10),
+                  Text(
+                    'Animal Type : ${snapshot.data['Species']}',
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Animal Breed : ${snapshot.data['Breed']}',
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Birth : ${DateFormat.yMMMd().format(snapshot.data['DOB'].toDate())}',
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                  ),
+                ]),
               ),
               ListTile(
                 title: Row(
